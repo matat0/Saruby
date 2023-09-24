1dcf39927b49735895043c0489630f73fe6fb781
matat0 <mattbangit@gmail.com>
2023-09-24 01:14:16 -0400

    updated player selectiopn, new fonts

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var player_current_attack = false
+
+#Character Variables - mato
+var PlayerSelect = 0
--- Scripts/playerselect.gd
+++ Scripts/playerselect.gd
@@ -1,23 +1,34 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimatedSprite2D.play("walkside")
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	match Global.PlayerSelect:
+		0:
+			get_node("PlayerSelect").play("walkside")
+			get_node("Des").text = "Matato"
+		1:
+			get_node("PlayerSelect").play("hidden")
+			get_node("Des").text = "hidden 1"
+		2:
+			get_node("PlayerSelect").play("hidden")
+			get_node("Des").text = "hidden 2"
+		3:
+			get_node("PlayerSelect").play("hidden")
+			get_node("Des").text = "hidden 3"
 
 func _on_left_pressed():
-	pass # Replace with function body.
+	if Global.PlayerSelect > 0:
+		Global.PlayerSelect -= 1
 
 
 func _on_right_pressed():
-	pass # Replace with function body.
-
+	if Global.PlayerSelect < 3:
+		Global.PlayerSelect += 1
 
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
