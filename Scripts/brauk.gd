8a8717f83de13185d588ff8a994bee580ee34505
beanlock <145821604+beanlock@users.noreply.github.com>
2023-09-26 02:49:27 -0400

    New animations mapped, Animations moved to state tree, brauk aggroes if hit,
    the way blend_positions are determind is insanely wrong, but it works for now ig

--- Scripts/brauk.gd
+++ Scripts/brauk.gd
@@ -1,24 +1,70 @@
 extends CharacterBody2D
-
-@onready var sprite = $AnimatedSprite2D
-
-
+var frame_velocity
+@onready var death_timer = $"boss death scene timer"
+@onready var prev_position : Vector2 = Vector2.ZERO
+@onready var direction : Vector2 = Vector2.ZERO
+@onready var sprite = $body
+@onready var animation_tree : AnimationTree = $AnimationTree
+var getting_hurt = false
 var speed = 200 #this value is inverse to the actual speed (higher # = slower movement)
 var player_chase = false
 var player = null
-var health = 5000
+var health:
+	set(new_health):
+		health = new_health
+		print("set new health")
+		if new_health > 0 and new_health < 5000:
+			_on_damage_taken()
+			
+	get:
+		return health
+		
 var player_in_range = false
 signal enemy_defeated
 var loot = 500
 
 
 
-func _on_enemy_defeated(loot: int):
+func _update_animation_parameters():
+	if(frame_velocity != Vector2.ZERO):
+		animation_tree["parameters/hurt/blend_position"] = frame_velocity
+		animation_tree["parameters/idle/blend_position"] = frame_velocity
+		animation_tree["parameters/walk/blend_position"] = frame_velocity
+
+		
+	if Global.boss_slain:
+		animation_tree["parameters/conditions/enemy_defeated"] = true
+		
+
+	if(getting_hurt):
+		animation_tree["parameters/conditions/is_hurt"] = true
+		getting_hurt = false
+		
+	else:
+		animation_tree["parameters/conditions/is_hurt"] = false
+		
+	if(frame_velocity == Vector2.ZERO):
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	else:
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+	
+
+func _on_enemy_defeated():
 	pass
 	
 
+func _on_damage_taken():
+	getting_hurt = true
+	$aggro_range/CollisionShape2D.scale = Vector2(10,10) #scales aggro range by 10 when enemy loses/gains health
 func _physics_process(delta):
 	#deal_with_damage() old melee function
+	var current_position = global_position
+	frame_velocity = (current_position - prev_position) / delta
+	prev_position = current_position
+
+	_update_animation_parameters()
 	if player_chase and health > 0:
 		var direction_to_player = (player.position - position).normalized()
 		if direction_to_player.x < 0:
@@ -26,37 +72,48 @@ func _physics_process(delta):
 		
 		elif direction_to_player.x > 0:
 			sprite.flip_h = true
-		
+			
+		elif direction_to_player.length() > 0.01:
+			#sprite.play("braukwalk")
+			pass
 		position += (player.position - position)/speed
+		#if getting_hurt:
+		#	sprite.play("braukhurt")
+			
+		#	getting_hurt = false
 		
-		if direction_to_player.length() > 0.01:
-			sprite.play("braukwalk")
 
-	elif health <= 0:
+	elif health <= 0 and Global.boss_slain == false:
 		health = 0
 		Global.boss_slain = true
-		sprite.play("braukdeath")
+		death_timer.start()
+		print("started death timer")
+		#sprite.play("braukdeath")
 		
 		
 	
 	else:
-		sprite.play("idle")
+		#sprite.play("idle")
+		pass
 
 
 func _on_animated_sprite_2d_animation_finished():
-	print("GZ u killed brauk")
-	emit_signal("enemy_defeated", loot)
-	print("signal emitted, ", loot, Global.score)
-	self.queue_free()
-	
+	if health == 0:
+		print("GZ u killed brauk")
+		emit_signal("enemy_defeated", loot)
+		print("signal emitted, ", loot, Global.score)
+		self.queue_free()
+
+
 
 func enemy():
 	pass
 
 func _ready():
 	Global.enemy = $"."
-	sprite.play("idle")
-
+	#sprite.play("idle")
+	animation_tree.active = true
+	
 
 func _on_aggro_range_body_entered(body):
 	player = body
@@ -64,19 +121,19 @@ func _on_aggro_range_body_entered(body):
 	
 
 
-func _on_aggro_range_body_exited(body):
-	player = null
-	player_chase = false
+#func _on_aggro_range_body_exited(body):
+#	player = null
+#	player_chase = false
 
 
-func _on_brauk_hitbox_body_entered(body):
+#func _on_brauk_hitbox_body_entered(body):
 	if body.has_method("player"):
 		player_in_range = true
 
 
-func _on_brauk_hitbox_body_exited(body):
-	if body.has_method("player"):
-		player_in_range = false
+#func _on_brauk_hitbox_body_exited(body):
+#	if body.has_method("player"):
+#		player_in_range = false
 
 #func deal_with_damage():    old meleee function, only keeping it for future melee attack reference
 #	if player_in_range and Global.player_current_attack:
@@ -84,3 +141,8 @@ func _on_brauk_hitbox_body_exited(body):
 #		print("brauk health =", health)
 #		
 
+
+
+func _on_boss_death_scene_timer_timeout():
+	print("death scene timer done")
+	queue_free()
