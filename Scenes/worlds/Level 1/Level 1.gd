4bd231a3dd022652d6067593814e298d71deb3ce
thebeegyoshi <jasonyi409@gmail.com>
2023-12-22 22:58:22 -0500

    Started Level 1 Design
    Got some tilesets and shit together.

---
+++ Scenes/worlds/Level 1/Level 1.gd
@@ -0,0 +1,14 @@
+"Developer's Note: The wall that blocks off the first boss room is going to be animated to collapse
+into the bottom wall."
+
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
