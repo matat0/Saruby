4c172ddeff6c4eeb2604f3aa756b277d7ab78e44
beanlock <145821604+ygeb1@users.noreply.github.com>
2025-02-13 03:01:15 -0500

    Update game.gd

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -3,11 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
-	var current_player = Global.characters[Global.PlayerSelect]
-	var gamer = current_player.instantiate()
-	gamer.global_position = Vector2(600,700)
-	add_child(gamer)
-	Global.gamer = gamer
+	var current_player = Global.characters[Global.PlayerSelect] #which character  did the user select?
+	var gamer = current_player.instantiate()         #instantiates actual gamer node corresponding to selection
+	gamer.global_position = Vector2(600,700)         #declares gamer's position on map
+	add_child(gamer)         
+	Global.gamer = gamer     
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
