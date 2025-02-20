44ef5985c22b700ab3ffcc9dd496c41678a76947
matat0 <mattbangit@gmail.com>
2025-02-20 15:32:14 -0500

    more sound effects

--- arcane_wave.gd
+++ arcane_wave.gd
@@ -5,14 +5,15 @@ var target
 var speed = 350
 var pathName = ""
 var bulletDamage
+var impacted = false
 
 func _ready():
 	$arcane_wave.play("arcane_wave")
-
+	$cast.play()
 
 func _physics_process(_delta):
 	
-	if target_position:
+	if target_position and not impacted:
 		var direction = (target_position - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
@@ -22,4 +23,10 @@ func _physics_process(_delta):
 func _on_area_2d_body_entered(body):
 	if body.has_method("enemy"):
 		body.health -= bulletDamage
+		impacted=true
+		$arcane_wave.queue_free()
+		$impact.play()
+		
+func _on_impact_finished() -> void:
+	if impacted:
 		self.queue_free()
--- phoenix.gd
+++ phoenix.gd
@@ -5,14 +5,16 @@ var target
 var speed = 350
 var pathName = ""
 var bulletDamage
+var impacted = false
 
 func _ready():
 	$phoenix.play("phoenix")
+	$cast.play()
 
 
 func _physics_process(_delta):
 	
-	if target_position:
+	if target_position and not impacted:
 		var direction = (target_position - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
@@ -22,4 +24,9 @@ func _physics_process(_delta):
 func _on_area_2d_body_entered(body):
 	if body.has_method("enemy"):
 		body.health -= bulletDamage
+		impacted = true
+		$phoenix.queue_free()
+
+func _on_cast_finished() -> void:
+	if impacted:
 		self.queue_free()
