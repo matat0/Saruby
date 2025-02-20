09c0ed209e339ed09bb7a93080cd30593e52eefd
matat0 <mattbangit@gmail.com>
2025-02-20 15:12:28 -0500

    added sound fx thanks to mikey

--- Scripts/proj_frost_orb.gd
+++ Scripts/proj_frost_orb.gd
@@ -12,7 +12,8 @@ func _ready():
 	var latent_arcana_proc = randi_range(1,4)
 	if latent_arcana_proc == 4:
 		get_parent().get_parent().latent_arcana_charges += 1
-		
+	
+	$cast.play()
 	
 func _physics_process(_delta):
 	
@@ -26,6 +27,11 @@ func _on_area_2d_body_entered(body):
 	if body.has_method("enemy"):
 		body.health -= bulletDamage
 		$FrostOrb.play("orb_impact")
-
+		$frost_orb_ring.queue_free()
+		$impact.play()
+	
 func _on_frost_orb_animation_finished():
-	self.queue_free()
+	$FrostOrb.queue_free()
+
+func _on_impact_finished() -> void:
+	$impact.queue_free()
